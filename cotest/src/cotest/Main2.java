00601c4118c1968d85830770b0f716d49821d2fc
user <user@DESKTOP-HLI0U8B>
2020-08-25 18:05:53 +0900

    for CT

---
+++ cotest/src/cotest/Main2.java
@@ -0,0 +1,31 @@
+package cotest;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main2 {
+
+public static void main(String[] args) throws Exception{
+	
+	Scanner sc= new Scanner(System.in);
+	 
+	int n =sc.nextInt();
+
+	   for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n - i; ++j) {
+                System.out.print(" ");
+            }
+             
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+	sc.close();
+}
+}
